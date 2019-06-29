08f768739548cfbd6d1986921cd3109bcd74b1e6
verxirtam <d-kosuga@jcom.home.ne.jp>
2019-06-30 02:35:23 +0900

    update dotfiles.

--- bashrc_set_ps1.sh
+++ bashrc_set_ps1.sh
@@ -6,10 +6,12 @@
 PS1_2_USERHOST_COLOR='00;38;05;204'
 # 区切り文字(:)
 PS1_3_SEPARATOR_COLOR='00;38;05;204'
-# カレントディレクトリ$
+# カレントディレクトリ
 PS1_4_DIR_COLOR='00;38;05;204'
-# 半角スペース, $
-PS1_5_DOLLER_COLOR='00;38;05;204'
+# gitステータス
+PS1_5_GIT_STATUS_COLOR='00;38;05;123'
+# $
+PS1_6_DOLLER_COLOR='00;38;05;204'
 
 # daisuke-lubuntu
 if [ $(uname -n) = "daisuke-lubuntu" ]; then
@@ -19,8 +21,8 @@ if [ $(uname -n) = "daisuke-lubuntu" ]; then
 	PS1_3_SEPARATOR_COLOR='00'
 	# カレントディレクトリ$
 	PS1_4_DIR_COLOR='00'
-	# 半角スペース, $
-	PS1_5_DOLLER_COLOR='00'
+	# $
+	PS1_6_DOLLER_COLOR='00'
 fi
 
 # dev (docker in daisuke-lubuntu)
@@ -31,8 +33,8 @@ if [ $(uname -n) = "dev" ]; then
 	PS1_3_SEPARATOR_COLOR='00;38;05;227'
 	# カレントディレクトリ$
 	PS1_4_DIR_COLOR='00;38;05;227'
-	# 半角スペース, $
-	PS1_5_DOLLER_COLOR='00'
+	# $
+	PS1_6_DOLLER_COLOR='00'
 fi
 
 # daisuke-raspi01
@@ -43,8 +45,8 @@ if [ $(uname -n) = "daisuke-raspi01" ]; then
 	PS1_3_SEPARATOR_COLOR='00;32'
 	# カレントディレクトリ$
 	PS1_4_DIR_COLOR='00;32'
-	# 半角スペース, $
-	PS1_5_DOLLER_COLOR='00'
+	# $
+	PS1_6_DOLLER_COLOR='00'
 fi
 
 # daisuke-raspi02
@@ -55,8 +57,8 @@ if [ $(uname -n) = "daisuke-raspi02" ]; then
 	PS1_3_SEPARATOR_COLOR='00;38;05;87'
 	# カレントディレクトリ$
 	PS1_4_DIR_COLOR='00;38;05;87'
-	# 半角スペース, $
-	PS1_5_DOLLER_COLOR='00'
+	# $
+	PS1_6_DOLLER_COLOR='00'
 fi
 
 # daisuke-letsnote
@@ -67,8 +69,8 @@ if [ $(uname -n) = "daisuke-letsnote" ]; then
 	PS1_3_SEPARATOR_COLOR='00;38;05;211'
 	# カレントディレクトリ$
 	PS1_4_DIR_COLOR='00;38;05;211'
-	# 半角スペース, $
-	PS1_5_DOLLER_COLOR='00'
+	# $
+	PS1_6_DOLLER_COLOR='00'
 fi
 
 
@@ -80,12 +82,14 @@ PS1_1_ROOT='${debian_chroot:+($debian_chroot)}'
 PS1_2_USERHOST='\[\e[${PS1_2_USERHOST_COLOR}m\]\u@\h\[\e[00m\]'
 # 区切り文字(:)
 PS1_3_SEPARATOR='\[\e[${PS1_3_SEPARATOR_COLOR}m\]:\[\e[00m\]'
-# カレントディレクトリ$
+# カレントディレクトリ
 PS1_4_DIR='\[\e[${PS1_4_DIR_COLOR}m\]\w\[\e[00m\]'
-# 半角スペース, $
-PS1_5_DOLLER='\[\e[${PS1_5_DOLLER_COLOR}m\] \$\[\e[00m\]'
+# gitステータス
+PS1_5_GIT_STATUS='\[\e[${PS1_5_GIT_STATUS_COLOR}m\]$(__git_ps1)\[\e[00m\]'
+# $
+PS1_6_DOLLER='\[\e[${PS1_6_DOLLER_COLOR}m\]\$\[\e[00m\]'
 # スペース
-PS1_6_SPACE=' '
+PS1_7_SPACE=' '
 
 # PS1の設定
 
@@ -93,20 +97,37 @@ PS1=${PS1_1_ROOT}
 PS1=$PS1${PS1_2_USERHOST}
 PS1=$PS1${PS1_3_SEPARATOR}
 PS1=$PS1${PS1_4_DIR}
-PS1=$PS1${PS1_5_DOLLER}
-PS1=$PS1${PS1_6_SPACE}
+PS1=$PS1${PS1_5_GIT_STATUS}
+PS1=$PS1${PS1_6_DOLLER}
+PS1=$PS1${PS1_7_SPACE}
 
 # 使用しない環境変数の開放
 
 #unset PS1_2_USERHOST_COLOR
 #unset PS1_3_SEPARATOR_COLOR
 #unset PS1_4_DIR_COLOR
-#unset PS1_5_DOLLER_COLOR
+#unset PS1_6_DOLLER_COLOR
 
 unset PS1_1_ROOT
 unset PS1_2_USERHOST
 unset PS1_3_SEPARATOR
 unset PS1_4_DIR
-unset PS1_5_DOLLER
-unset PS1_6_SPACE
+unset PS1_6_DOLLER
+unset PS1_7_SPACE
+
+#
+# git-completion.bash / git-prompt.sh
+#
+if [ -f ./tools/git/git-completion.bash ]; then
+    source ./tools/git/git-completion.bash
+fi
+if [ -f ./tools/git/git-prompt.sh ]; then
+    source ./tools/git/git-prompt.sh
+fi
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUPSTREAM=auto
+
+
 
