f65600af93b1558163ca6751ba20867467b293b1
verxirtam <verxirtam@github.com>
2019-12-13 11:36:53 +0900

    update dotfiles.

--- bashrc_specific.sh
+++ bashrc_specific.sh
@@ -23,6 +23,21 @@ fi
 
 
 ###################################
+# dev
+###################################
+if [ $(uname -n) = "dev" ]; then
+	
+	#PATHの設定
+	#$SHLVL = 1 の時のみ実施
+	if [ $SHLVL = 1 ]; then
+		# rust
+		export PATH="$HOME/.cargo/bin:$PATH"
+		:
+	fi
+fi
+
+
+###################################
 # daisuke-jetson
 ###################################
 if [ $(uname -n) = "daisuke-jetson" ]; then
