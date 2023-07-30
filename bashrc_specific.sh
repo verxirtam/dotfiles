0123b5cb344cf4bb5a01253e56d29d42d85adf11
verxirtam <d.kosuga@gmail.com>
2023-07-30 22:16:08 +0900

    update dotfiles.

--- bashrc_specific.sh
+++ bashrc_specific.sh
@@ -4,21 +4,22 @@
 # daisuke-msipc
 ###################################
 if [ $(uname -n) = "daisuke-msipc" ]; then
+	: "setting for daisuke-msipc"
 	# jdk
-	export JAVA_HOME=/usr/java/jdk1.8.0_20
-	export CLASSPATH=.:/usr/java
+	# export JAVA_HOME=/usr/java/jdk1.8.0_20
+	# export CLASSPATH=.:/usr/java
 	
 	#PATHの設定
 	#$SHLVL = 1 の時のみ実施
-	if [ $SHLVL = 1 ]; then
-		#jdk
-		export PATH=/usr/java/jdk1.8.0_20/bin:$PATH
-		#eclipse
-		export PATH=/usr/local/eclipse:$PATH
-		#STS setting 2015/12/27
-		export PATH=/usr/local/sts-bundle/sts-3.7.2.RELEASE:$PATH
-		:
-	fi
+	# if [ $SHLVL = 1 ]; then
+	#	#jdk
+	#	export PATH=/usr/java/jdk1.8.0_20/bin:$PATH
+	#	#eclipse
+	#	export PATH=/usr/local/eclipse:$PATH
+	#	#STS setting 2015/12/27
+	#	export PATH=/usr/local/sts-bundle/sts-3.7.2.RELEASE:$PATH
+	#	:
+	#fi
 fi
 
 
