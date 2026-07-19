2310d83156564fb225d9507af5a01792a54a4d49
verxirtam <d.kosuga@gmail.com>
2026-07-20 02:36:57 +0900

    ホスト別設定をhosts/<hostname>/配下に分割し、複雑化した分岐を整理
    bashrc_set_ps1.sh・bashrc_specific.sh・.tmux.conf・.tmux.sessionに
    散在していたホスト名分岐(uname -n)を、hosts/<hostname>/配下の
    ファイルに集約。新規マシン追加時は既存4ファイルを編集せず
    hosts/配下にディレクトリを追加するだけで済むようにした。
    
    あわせてtmux起動時に発生した以下の不具合を修正:
    - pane-border-fg/pane-active-border-fgがtmux 2.9で廃止されており
      invalid optionエラーになっていたためpane-border-style等に統一
    - source-fileが-F無しでは#{host}をフォーマット展開しないため
      ホスト別tmux.confが読み込まれていなかった問題を修正
    - tmux setenvで設定していたPREFIX変数がnvmの予約変数と衝突して
      いたためTMUX_PREFIX_KEYに改名

--- bashrc_specific.sh
+++ bashrc_specific.sh
@@ -1,88 +1,10 @@
 #!/bin/bash
 
-###################################
-# daisuke-lubuntu
-###################################
-if [ $(uname -n) = "daisuke-lubuntu" ]; then
-	# jdk
-	export JAVA_HOME=/usr/java/jdk1.8.0_20
-	export CLASSPATH=.:/usr/java
-	
-	#PATHの設定
-	#$SHLVL = 1 の時のみ実施
-	if [ $SHLVL = 1 ]; then
-		#jdk
-		export PATH=/usr/java/jdk1.8.0_20/bin:$PATH
-		#eclipse
-		export PATH=/usr/local/eclipse:$PATH
-		#STS setting 2015/12/27
-		export PATH=/usr/local/sts-bundle/sts-3.7.2.RELEASE:$PATH
-		:
-	fi
+# ホスト固有の設定
+# hosts/<hostname>/specific.sh が存在すればそれを読み込む
+HOST_SPECIFIC_CONFIG=~/tools/dotfiles/hosts/$(uname -n)/specific.sh
+if [ -f "$HOST_SPECIFIC_CONFIG" ]; then
+	source "$HOST_SPECIFIC_CONFIG"
 fi
-
-
-###################################
-# dev
-###################################
-if [ $(uname -n) = "dev" ]; then
-	
-	#PATHの設定
-	#$SHLVL = 1 の時のみ実施
-	if [ $SHLVL = 1 ]; then
-		# rust
-		export PATH="$HOME/.cargo/bin:$PATH"
-		:
-	fi
-fi
-
-
-###################################
-# daisuke-jetson
-###################################
-if [ $(uname -n) = "daisuke-jetson" ]; then
-	
-	#PATHの設定
-	#$SHLVL = 1 の時のみ実施
-	if [ $SHLVL = 1 ]; then
-		# CUDA
-		export PATH=/usr/local/cuda/bin:${PATH}
-		export LD_LIBRARY_PATH=/usr/local/cuda/lib64:${LD_LIBRARY_PATH}
-		:
-	fi
-fi
-
-
-###################################
-# daisukenuc
-###################################
-if [ $(uname -n) = "daisukenuc" ]; then
-	
-	# obsidian mcp向けの証明書設定
-	export NODE_EXTRA_CA_CERTS="$HOME/obsidian-cert.pem"
-	
-	#$SHLVL = 1 の時のみ実施
-	if [ $SHLVL = 1 ]; then
-		sageenv () {
-			source ~/miniforge3/bin/activate
-			conda activate sage
-		}
-		
-		sagecode () {
-			source ~/miniforge3/bin/activate
-			conda activate sage
-			code .
-		}
-		sagejupyter () {
-			source ~/miniforge3/bin/activate
-			conda activate sage
-			jupyter lab
-		}
-		globalenv () {
-			while [ ! -z "$CONDA_DEFAULT_ENV" ]; do
-			conda deactivate
-			done
-		}
-	fi
-fi
+unset HOST_SPECIFIC_CONFIG
 
