a15e848f608fad8ae419c880973b442634ce6b72
verxirtam <d.kosuga@gmail.com>
2026-07-20 01:34:11 +0900

    update vimrc and bashrc_specific.sh

--- bashrc_specific.sh
+++ bashrc_specific.sh
@@ -52,3 +52,37 @@ if [ $(uname -n) = "daisuke-jetson" ]; then
 	fi
 fi
 
+
+###################################
+# daisukenuc
+###################################
+if [ $(uname -n) = "daisukenuc" ]; then
+	
+	# obsidian mcp向けの証明書設定
+	export NODE_EXTRA_CA_CERTS="$HOME/obsidian-cert.pem"
+	
+	#$SHLVL = 1 の時のみ実施
+	if [ $SHLVL = 1 ]; then
+		sageenv () {
+			source ~/miniforge3/bin/activate
+			conda activate sage
+		}
+		
+		sagecode () {
+			source ~/miniforge3/bin/activate
+			conda activate sage
+			code .
+		}
+		sagejupyter () {
+			source ~/miniforge3/bin/activate
+			conda activate sage
+			jupyter lab
+		}
+		globalenv () {
+			while [ ! -z "$CONDA_DEFAULT_ENV" ]; do
+			conda deactivate
+			done
+		}
+	fi
+fi
+
